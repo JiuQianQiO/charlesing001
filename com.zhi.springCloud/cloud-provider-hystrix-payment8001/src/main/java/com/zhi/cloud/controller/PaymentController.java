c729936756c47230554284a648fc2aef53f594ca
charlesing <wangzhi_email@163.com>
2021-09-26 22:54:04 +0800

    springCloud

--- com.zhi.springCloud/cloud-provider-hystrix-payment8001/src/main/java/com/zhi/cloud/controller/PaymentController.java
+++ com.zhi.springCloud/cloud-provider-hystrix-payment8001/src/main/java/com/zhi/cloud/controller/PaymentController.java
@@ -1,14 +1,19 @@
 package com.zhi.cloud.controller;
 
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.zhi.cloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.UUID;
 
 @RestController
 @Slf4j
@@ -34,4 +39,12 @@ public class PaymentController {
         log.info("timeOut>>>>>>>>>>>>>>>>result: "+result+":::     :serverPort"+serverPort);
         return result;
     }
+//==========服务熔断
+    @GetMapping("/payment/circuit/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") int id){
+        String circuitBreak = paymentService.paymentCircuitBreak(id);
+        log.info("*******circuitBreaker"+circuitBreak);
+        return circuitBreak;
+    }
+
 }
--- com.zhi.springCloud/cloud-provider-hystrix-payment8001/src/main/java/com/zhi/cloud/service/PaymentService.java
+++ com.zhi.springCloud/cloud-provider-hystrix-payment8001/src/main/java/com/zhi/cloud/service/PaymentService.java
@@ -4,7 +4,9 @@ package com.zhi.cloud.service;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -44,4 +46,34 @@ public class PaymentService {
     public String paymentInfo_TimeOutFallback(int id){
         return "线程池：    "+Thread.currentThread().getName() + "系统繁忙，稍后再试       paymentInfo_TimeOutFallback, id:     "+ id + "\t"+"🆗";
     }
+
+    //============================================================================================
+    //-----服务熔断实例--------
+
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback",commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled",value = "true"),// 设置是否开启断路器
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"),//设置请求次数
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"),// 设置时间窗口期，
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60"),// 失败率达到多少后跳闸
+    })
+    // 上面配置的含义是：
+    //          第一个参数：当发生服务熔断的时候，调用 paymentCircuitBreaker_fallback 方法
+    //          第二个参数： 1、配置开启服务熔断；
+    //          第二、四个参数： 2、4、请求10次以上，失败率达到 60% 后开启调用服务熔断的方法，进行服务降级,
+    //          第三个参数：在进行服务熔断以后，在时间窗口期内输入正确的地址也不能返回正确的页面，除非在时间窗口器访问的正确率上来了，就会回复访问
+
+
+    public String paymentCircuitBreak(@PathVariable("id") int id){
+        if (id < 0){
+            throw  new RuntimeException("**************id 不能为负数，请稍后再试");
+        }
+        String serialNumber = String.valueOf(UUID.randomUUID());
+        return Thread.currentThread().getName()+"\t"+"调用成功，流水号："+serialNumber;
+    }
+
+    //服务熔断后，fallback（服务降级）的方法
+    public String paymentCircuitBreaker_fallback(@PathVariable("id") int id){
+        return "您请求的 ID  为负数，请稍后再试！！ <( _ _ )> （＞人＜；）:    "+id;
+    }
+
 }
